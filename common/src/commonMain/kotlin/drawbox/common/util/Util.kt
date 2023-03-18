4fa01b179fa2f901a143a04a4ada320a86628344
Mark Yavorskyi <yavorskiymark@gmail.com>
2023-03-18 22:02:07 +0200

    Function return type refactored

--- common/src/commonMain/kotlin/drawbox/common/util/Util.kt
+++ common/src/commonMain/kotlin/drawbox/common/util/Util.kt
@@ -3,23 +3,25 @@ package drawbox.common.util
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Path
 
-fun createPath(points: List<Offset>) = Path().apply {
-    if (points.size > 1) {
-        var oldPoint: Offset? = null
-        this.moveTo(points[0].x, points[0].y)
-        for (i in 1 until points.size) {
-            val point: Offset = points[i]
-            oldPoint?.let {
-                val midPoint = calculateMidpoint(it, point)
-                if (i == 1) {
-                    this.lineTo(midPoint.x, midPoint.y)
-                } else {
-                    this.quadraticBezierTo(it.x, it.y, midPoint.x, midPoint.y)
+fun createPath(points: List<Offset>): Path {
+    return Path().apply {
+        if (points.size > 1) {
+            var oldPoint: Offset? = null
+            this.moveTo(points[0].x, points[0].y)
+            for (i in 1 until points.size) {
+                val point: Offset = points[i]
+                oldPoint?.let {
+                    val midPoint = calculateMidpoint(it, point)
+                    if (i == 1) {
+                        this.lineTo(midPoint.x, midPoint.y)
+                    } else {
+                        this.quadraticBezierTo(it.x, it.y, midPoint.x, midPoint.y)
+                    }
                 }
+                oldPoint = point
             }
-            oldPoint = point
+            oldPoint?.let { this.lineTo(it.x, oldPoint.y) }
         }
-        oldPoint?.let { this.lineTo(it.x, oldPoint.y) }
     }
 }
 
