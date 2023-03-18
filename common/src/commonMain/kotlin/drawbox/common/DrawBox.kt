6d8120c73431df34e376e650b4b15744caafa06a
Mark Yavorskyi <yavorskiymark@gmail.com>
2023-03-18 21:58:30 +0200

    Implemented DrawBox

--- common/src/commonMain/kotlin/drawbox/common/DrawBox.kt
+++ common/src/commonMain/kotlin/drawbox/common/DrawBox.kt
@@ -1,12 +1,40 @@
 package drawbox.common
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
 
 @Composable
 fun DrawBox(
     controller: DrawController,
     modifier: Modifier = Modifier.fillMaxSize(),
 ) {
+    val bitmap by remember { controller.drawnBitmapState }
+
+    Canvas(modifier = modifier
+        .onSizeChanged { newSize -> controller.connectToDrawBox(newSize) }
+        .pointerInput(Unit) {
+            detectTapGestures(
+                onTap = { offset ->
+                    controller.insertNewPath(offset)
+                    controller.updateLatestPath(offset)
+                }
+            )
+        }
+        .pointerInput(Unit) {
+            detectDragGestures(
+                onDragStart = { offset -> controller.insertNewPath(offset) },
+                onDrag = { change, _ -> controller.updateLatestPath(change.position) }
+            )
+        }
+    ) {
+        bitmap?.let { bitmap ->
+            drawImage(bitmap)
+        }
+    }
 }
