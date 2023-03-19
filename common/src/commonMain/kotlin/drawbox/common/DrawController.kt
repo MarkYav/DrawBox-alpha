bf3962e9561384dd324fd26751ec8f570c72246b
Mark Yavorskyi <yavorskiymark@gmail.com>
2023-03-19 22:08:39 +0200

    Added documentation

--- common/src/commonMain/kotlin/drawbox/common/DrawController.kt
+++ common/src/commonMain/kotlin/drawbox/common/DrawController.kt
@@ -8,23 +8,44 @@ import drawbox.common.model.PathWrapper
 import drawbox.common.util.createPath
 import drawbox.common.util.pop
 
+/**
+ * DrawController interacts with [DrawBox] and it allows you to control the canvas and all the components with it.
+ */
 class DrawController {
+    /** A stateful list of [Path] that is drawn on the [Canvas]. */
     private val drawnPaths = mutableStateListOf<PathWrapper>()
+
+    /** A stateful list of [Path] that was drawn on the [Canvas] but user retracted his action. */
     private val canceledPaths = mutableStateListOf<PathWrapper>()
 
+    /** An [ImageBitmap] to draw paths on the [canvas]. */
     private var _drawnBitmap: ImageBitmap? = null
+
+    /** A [Canvas] to change the [_drawnBitmap] while we are drawing on it. */
     private var canvas: Canvas? = null
 
+    /** A private state of the bitmap to be drawn */
     private val _drawnBitmapState = mutableStateOf<ImageBitmap?>(null)
+
+    /** A public state of the bitmap to be drawn */
     val drawnBitmapState = _drawnBitmapState as State<ImageBitmap?>
 
+    /** An [opacity] of the stroke */
     var opacity by mutableStateOf(1f)
+
+    /** A [strokeWidth] of the stroke */
     var strokeWidth by mutableStateOf(10f)
+
+    /** A [color] of the stroke */
     var color by mutableStateOf(Color.Red)
 
+    /** Indicate how many redos it is possible to do. */
     val undoCount by derivedStateOf { drawnPaths.size }
+
+    /** Indicate how many undos it is possible to do. */
     val redoCount by derivedStateOf { canceledPaths.size }
 
+    /** Executes undo the drawn path if possible. */
     fun undo() {
         if (drawnPaths.isNotEmpty()) {
             canceledPaths.add(drawnPaths.pop())
@@ -32,6 +53,7 @@ class DrawController {
         }
     }
 
+    /** Executes redo the drawn path if possible. */
     fun redo() {
         if (canceledPaths.isNotEmpty()) {
             drawnPaths.add(canceledPaths.pop())
@@ -39,17 +61,20 @@ class DrawController {
         }
     }
 
+    /** Clear drawn paths and the bitmap image. */
     fun reset() {
         drawnPaths.clear()
         canceledPaths.clear()
         invalidateBitmap()
     }
 
+    /** Call this function when user starts drawing a path. */
     internal fun updateLatestPath(newPoint: Offset) {
         drawnPaths.last().points.add(newPoint)
         invalidateBitmap()
     }
 
+    /** When dragging call this function to update the last path. */
     internal fun insertNewPath(newPoint: Offset) {
         val pathWrapper = PathWrapper(
             points = mutableStateListOf(newPoint),
@@ -61,6 +86,7 @@ class DrawController {
         canceledPaths.clear()
     }
 
+    /** Call this function to connect to the [DrawBox]. */
     internal fun connectToDrawBox(size: IntSize) {
         if (size.width > 0 && size.height > 0) {
             _drawnBitmap = ImageBitmap(size.width, size.height, ImageBitmapConfig.Argb8888)
@@ -68,6 +94,7 @@ class DrawController {
         }
     }
 
+    /** Updates the [_drawnBitmapState] and [_drawnBitmap] based on the [drawnPaths]. */
     private fun invalidateBitmap() {
         drawnPaths.forEach { pw ->
             canvas?.drawPath(
